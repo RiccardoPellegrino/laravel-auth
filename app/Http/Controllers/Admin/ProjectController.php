60958597fde81d78a314f3a1b367c8453bffbaec
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-01-10 17:30:35 +0100

    controller admin

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     
+     */
+    public function index()
+    {
+        $projects =Project::all();
+        return view('admin.projects.index',compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     
+     */
+    public function create()
+    {
+        return view('admin.projects.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+    
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+   
+     */
+    public function show($id)
+    {
+        return view('admin.projects.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     
+     */
+    public function edit($id)
+    {
+        return view('admin.projects.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
@@ -31,6 +32,7 @@ Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')
    ->group(function () {
          Route::get('/', [DashboardController::class, 'index'])
          ->name('dashboard');
+         Route::resource('projects',ProjectController::class)->parameters(['projects'=>'project:slug']);
    });
 
 require __DIR__.'/auth.php';
