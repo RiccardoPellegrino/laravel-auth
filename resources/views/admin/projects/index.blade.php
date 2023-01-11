f6ee943cbf3529926cc25357fde8eec8abd484ca
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-01-11 21:50:18 +0100

    modifiche index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,49 +2,55 @@
 
 @section('content')
     <h1>Project</h1>
-    <a class="btn btn-success" href="{{route('admin.projects.create')}}">Crea nuovo progetto</a>
-    @if(session()->has('message'))
-    <div class="alert alert-success mb-3 mt-3">
-        {{ session()->get('message') }}
-    </div>
+    <a class="btn btn-success" href="{{ route('admin.projects.create') }}">Crea nuovo progetto</a>
+    @if (session()->has('message'))
+        <div class="alert alert-success mb-3 mt-3">
+            {{ session()->get('message') }}
+        </div>
     @endif
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nome Progetto</th>
-            <th scope="col">Descrizione</th>
-            <th scope="col">Linguaggi</th>
-            <th scope="col">Framework</th>
-            <th scope="col">Team</th>
-            <th scope="col">Link Git</th>
-            <th scope="col">Difficolta</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($projects as $project)
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <th scope="row">{{$project->id}}</th>
-                    <td><a href="{{route('admin.projects.show', $project->slug)}}" title="View Post">{{$project->name_project}}</a></td>
-                    <td>{{Str::limit($project->description,100)}}</td>
-                    <td>{{$project->dev_lang}}</td>
-                    <td>{{$project->framework}}</td>
-                    <td>{{$project->team}}</td>
-                    <td>{{$project->link_git}}</td>
-                    <td>{{$project->lvl_diff}}</td>
-                    <td><a class="link-secondary" href="{{route('admin.projects.edit', $project->slug)}}" title="Edit Post"><i class="fa-solid fa-pen"></i></a></td>
-                    <td>
-                        <form action="{{route('admin.projects.destroy', $project->slug)}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="delete-button btn btn-danger ms-3" data-item-title="{{$project->name_project}}"><i class="fa-solid fa-trash-can"></i></button>
-                     </form>
-                    </td>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome Progetto</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Linguaggi</th>
+                    <th scope="col">Framework</th>
+                    <th scope="col">Team</th>
+                    <th scope="col">Link Git</th>
+                    <th scope="col">Difficolta</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
                 </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <td><a href="{{ route('admin.projects.show', $project->slug) }}"
+                                title="View Post">{{ $project->name_project }}</a></td>
+                        <td>{{ Str::limit($project->description, 100) }}</td>
+                        <td>{{ $project->dev_lang }}</td>
+                        <td>{{ $project->framework }}</td>
+                        <td>{{ $project->team }}</td>
+                        <td>{{ $project->link_git }}</td>
+                        <td>{{ $project->lvl_diff }}</td>
+                        <td><a class="link-secondary" href="{{ route('admin.projects.edit', $project->slug) }}"
+                                title="Edit Post"><i class="fa-solid fa-pen"></i></a></td>
+                        <td>
+                            <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="delete-button btn btn-danger ms-3"
+                                    data-item-title="{{ $project->name_project }}"><i
+                                        class="fa-solid fa-trash-can"></i></button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     @include('partials.admin.modal-delete')
 @endsection
